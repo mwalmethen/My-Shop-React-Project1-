9d3e89cef472ff34d3be569b5a94489e24fd2a75
Mohammed Almethen <almethen2002@gmail.com>
2024-11-11 01:42:36 +0300

    Modified src file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1 class = "Main-h1">The Cookie Jar</h1> 
+      <p class = "description"> &#10084; where cookie lovers unite! &#127850;</p>
+      <img class = "My-shop-image" src = "/My-shop-storefront.webp" alt = "My Shop"/>
+
     </div>
   );
 }
