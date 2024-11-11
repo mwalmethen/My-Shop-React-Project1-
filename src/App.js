586e647c6cb8a88e648f9cd96d97acc36768389f
Mohammed Almethen <almethen2002@gmail.com>
2024-11-11 23:09:12 +0300

    React Shop Page 3

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
 import './App.css';
 import products from './products';
+
 function App() {
+  const productsList = products.map((product) => (
+
+  <div className="div that has all the products in the products.js">
+  <img className="cookie-image" src={product.image} alt="cookies-image"/>
+  <p className="description-product1"> {product.name} </p>
+  <p className="description-product2"> {`${product.price} KD`} </p>
+
+  </div>
+));
+  
   return (
     <div className="App">
-      <h1 class = "Main-h1">The Cookie Jar</h1> 
-      <p class = "description"> &#10084; where cookie lovers unite! &#127850;</p>
-      <img class = "My-shop-image" src = "/My-shop-storefront.webp" alt = "My Shop"/>
+      <h1 className = "Main-h1">The Cookie Jar</h1> 
+      <p className = "description"> &#10084; where cookie lovers unite! &#127850;</p>
+      <img className = "My-shop-image" src = "/My-shop-storefront.webp" alt = "My Shop"/>
+
+
+      <div class="My-product">
+        {productsList}
+      </div>
       
-      
-        
-        <div class="My-product">
-          {products.map((product) =>
-
-          <div class="div that has all the products in the products.js">
-          <img class="cookie-image" src={product.image} alt="cookies-image"/>
-          <p class="description-product1"> {product.name} </p>
-          <p class="description-product2"> {`${product.price} KD`} </p>
-
-          </div>
-)}
-        </div>
-
-
-
-
-
-      
-
-
-
     </div>
   );
+
 }
+
 
 export default App;
