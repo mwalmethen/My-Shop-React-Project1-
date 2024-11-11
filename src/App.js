cc5af1a6b0ba1d7be75b7fb04fa704d228ce8cf4
Mohammed Almethen <almethen2002@gmail.com>
2024-11-11 22:38:13 +0300

    Task2-My-shp src

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
-
+import products from './products';
 function App() {
   return (
     <div className="App">
-
       <h1 class = "Main-h1">The Cookie Jar</h1> 
       <p class = "description"> &#10084; where cookie lovers unite! &#127850;</p>
       <img class = "My-shop-image" src = "/My-shop-storefront.webp" alt = "My Shop"/>
+      
+      
+        
+        <div class="My-product">
+          {products.map((product) =>
+
+          <div class="div that has all the products in the products.js">
+          <img class="cookie-image" src={product.image} alt="cookies-image"/>
+          <p class="description-product1"> {product.name} </p>
+          <p class="description-product2"> {`${product.price} KD`} </p>
+
+          </div>
+)}
+        </div>
+
+
+
+
+
+      
+
+
 
     </div>
   );
---
+++ src/products.js
@@ -0,0 +1,14 @@
+const products = [
+{
+    name: "Chocolate Chip Cookies",
+    price: 5, 
+    image: "/Choco Chip Cookie.jpg",
+},
+{
+    name: "Rainbow Cookies",
+    price: 7, 
+    image: "/rainbow-cookies.webp",
+},
+];
+
+export default products;
