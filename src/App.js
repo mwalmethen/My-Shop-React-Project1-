c8be08f16f7f70409de4b11e56f53830d90aa3f2
Mohammed Almethen <almethen2002@gmail.com>
2024-11-12 01:43:17 +0300

    React Part-4

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
-import products from './products';
+import ProductItem from './Components/Product-Item';
 
 function App() {
-  const productsList = products.map((product) => (
-
-  <div className="div that has all the products in the products.js">
-  <img className="cookie-image" src={product.image} alt="cookies-image"/>
-  <p className="description-product1"> {product.name} </p>
-  <p className="description-product2"> {`${product.price} KD`} </p>
-
-  </div>
-));
-  
   return (
     <div className="App">
       <h1 className = "Main-h1">The Cookie Jar</h1> 
       <p className = "description"> &#10084; where cookie lovers unite! &#127850;</p>
       <img className = "My-shop-image" src = "/My-shop-storefront.webp" alt = "My Shop"/>
 
-
-      <div class="My-product">
-        {productsList}
-      </div>
+      <div className="div that has all the products in the products.js">
+    <ProductItem/>
+    </div>
       
     </div>
   );
---
+++ src/Components/Product-Item.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ const ProductItem = () => {
+  return (
+    <div>
+  <img className="cookie-image" src="/Choco Chip Cookie.jpg" alt="cookies-image"/>
+  <p className="description-product1"> Chocolate Chip Cookie </p>
+  <p className="description-product2">  5 KD </p>
+    </div>
+  )
+}
+export default ProductItem; 
